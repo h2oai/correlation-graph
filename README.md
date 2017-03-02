5a03864978e9a8259a8ee5a614155e149ed29483
Micah Stubbs <micah.stubbs@gmail.com>
2017-03-02 13:13:12 -0800

    add project and configuration

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# correlation-graph
+a visualization that draws a correlation network from pairwise correlations calculated by the `vis-data-server` for the `airlines_all` dataset as aggregated by *h2o-3*
+
+draws from a graph.json file
+
+this iteration adds community detection with [jLouvain](https://github.com/upphiminn/jLouvain)
+
---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import json from 'rollup-plugin-json';
+import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import strip from 'rollup-plugin-strip';
+
+export default {
+  entry: 'src/index.js',
+  format: 'umd',
+  globals: {},
+  moduleName: 'correlation-graph',
+  plugins: [
+    nodeResolve({ jsnext: true, main: true }),
+    json(),
+    babel(),
+    commonjs(),
+    // strip({
+    //   // set this to `false` if you don't want to
+    //   // remove debugger statements
+    //   debugger: true,
+    //   // defaults to `[ 'console.*', 'assert.*']`
+    //   functions: [ 'console.log', 'assert.*', 'debug'],
+    //   // set this to `false` if you're not using sourcemaps –
+    //   // defaults to `true`
+    //   sourceMap: false
+    // })
+  ],
+  external: [],
+  dest: 'build/correlation-graph.js',
+  acorn: {
+    allowReserved: true
+  }//,
+  //sourceMap: true,
+  //sourceMapFile: 'build/correlation-graph.js'
+};
